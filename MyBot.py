eb44a8bbfbbebe22cd4e90fbe9c8c3a724f6f551
Justin Chau <justin.jay.chau@gmail.com>
2018-11-07 17:58:35 -0500

    Fixed Staying Still Issue

--- MyBot.py
+++ MyBot.py
@@ -6,15 +6,18 @@ from hlt import Position
 from hlt.positionals import Direction
 
 game = hlt.Game()
-game.ready("Justin-Halite-Bot")
+game.ready("Justin-Bot")
 logging.info("Initialized! ID = {}.".format(game.my_id))
 
-ship_paths = {} #Dict with paths for each ship.
+# Dictionary with ship_id as key and ship path.
+ship_paths = {}
 
 def find_best_position(ship):
+    # Returns the optimal position to set the target to per ship.
     optimal_position = ship.position
-    max_halite_value = game_map[ship.position].halite_amount
+    max_halite_value = game_map[ship.position].halite_amount #* 1.35
 
+    # Finds all current long term targets from paths.
     current_targets = []
     for path in ship_paths.values():
         if len(path) > 0:
@@ -26,7 +29,7 @@ def find_best_position(ship):
                 optimal_position = position
                 max_halite_value = game_map[position].halite_amount
 
-    if max_halite_value == 0:
+    if max_halite_value < 30:
         max_halite_value = 0
         for y in range(game_map.height):
             for x in range(game_map.width):
@@ -36,7 +39,8 @@ def find_best_position(ship):
                     if cell_value > max_halite_value:
                         max_halite_value = cell_value
                         optimal_position = cell.position
-            
+
+    logging.info("Ship {} has found position {}".format(ship.id, optimal_position))      
     return game_map.normalize(optimal_position)
 
 def a_star(start, target):
@@ -72,8 +76,11 @@ def a_star(start, target):
     for position in path:
         position_path.append(game_map.normalize(tuple_to_pos(position)))
 
-    if len(position_path) < 0:
+    del position_path[0] #Important: Prevents ships from staying in place during path change.
+
+    if len(position_path) < 1:
         position_path.append(game_map.normalize(target))
+        logging.info("Staying in place.")
 
     return position_path
 
@@ -89,16 +96,33 @@ def can_move_off(ship):
     else:
         return False
 
+def find_nearest_drop(ship):
+    optimal_position = me.shipyard.position
+    min_distance = game_map.calculate_distance(ship.position, me.shipyard.position)
+    for drop in me.get_dropoffs():
+        if game_map.calculate_distance(ship.position, drop.position) < min_distance:
+            optimal_position = drop.position
+            min_distance = game_map.calculate_distance(ship.position, drop.position)
+
+    return optimal_position
+
 while True:
     game.update_frame()
     me = game.me
     game_map = game.game_map
-
     command_queue = []
-
     next_positions = {}
-
     players = game.players
+    turn_halite = me.halite_amount
+
+    collided_ships = []
+    for ship_id in ship_paths:
+        if not me.has_ship(ship_id):
+            collided_ships.append(ship_id)
+
+    for ship_id in collided_ships:
+        ship_paths.pop(ship_id)
+        logging.info("Removing collided ship paths for ship: {}".format(ship_id))
 
     opposing_locations = []
     for player in players:
@@ -115,8 +139,9 @@ while True:
                 has_current_path = True
 
         if has_current_path == False:
-            if ship.halite_amount > 750 and not game_map[me.shipyard.position].is_occupied:
-                ship_paths[ship.id] = a_star(ship.position, me.shipyard.position)
+            if ship.halite_amount > 750:
+                nearest_drop = find_nearest_drop(ship)
+                ship_paths[ship.id] = a_star(ship.position, nearest_drop)
             else:
                 ship_paths[ship.id] = a_star(ship.position, find_best_position(ship))
 
@@ -148,42 +173,6 @@ while True:
             else:
                 next_adj_positions[ship_id] = next_positions[ship_id]
 
-    #     if next_positions[ship_id] == me.get_ship(ship_id).position:
-    #         next_adj_positions[ship_id] = me.get_ship(ship_id).position
-    #     elif next_positions[ship_id] not in next_adj_positions.values():
-    #         next_adj_positions[ship_id] = next_positions[ship_id]
-    #     else:
-    #         next_adj_positions[ship_id] = me.get_ship(ship_id).position
-            
-    # positions = []
-    # duplicate_positions = []
-    # for ship_id in next_adj_positions:
-    #     if pos_to_tuple(next_adj_positions[ship_id]) not in positions:
-    #         positions.append(pos_to_tuple(next_adj_positions[ship_id]))
-    #     else:
-    #         duplicate_positions.append(next_adj_positions[ship_id])
-    #         logging.info("Duplicate positions found: {}".format(duplicate_positions))
-
-    # duplicate_ships = []
-    # for duplicate in duplicate_positions:
-    #     for ship_id, position in next_adj_positions.items():    # for name, age in dictionary.iteritems():  (for Python 2.x)
-    #         if position == duplicate:
-    #             if next_adj_positions[ship_id] != me.get_ship(ship_id).position:
-    #                 next_adj_positions[ship_id] = me.get_ship(ship_id).position
-    #                 logging.info("Duplicate Avoided!")
-
-    # for ship in me.get_ships():
-    #     if ship.id not in ship_paths:
-    #         next_adj_positions[ship.id] = ship.position
-    #     elif ship.position == next_positions[ship.id]:
-    #         next_adj_positions[ship.id] = ship.position
-
-    # for ship_id in next_positions:
-    #     if next_positions[ship_id] not in next_adj_positions.values():
-    #         next_adj_positions[ship_id] = next_positions[ship_id]
-    #     else:
-    #         next_adj_positions[ship_id] = me.get_ship(ship_id).position
-
     logging.info("Next Adjusted Positions: {}".format(next_adj_positions))
 
     for ship_id in next_adj_positions:
@@ -194,7 +183,7 @@ while True:
                 if next_adj_positions[ship_id] == ship_paths[ship_id][0]:
                     ship_paths[ship_id].pop(0)
 
-    if game.turn_number <= 200 and me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied and not me.shipyard.position in next_adj_positions.values():
+    if game.turn_number <= 200 and turn_halite >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied and not me.shipyard.position in next_adj_positions.values():
         command_queue.append(me.shipyard.spawn())
         
     logging.info("Current Paths (Turn End): {}".format(ship_paths))
--- OldBot.py
+++ OldBot.py
@@ -1,73 +1,202 @@
 import hlt
 import logging
+from queue import *
 from hlt import constants
 from hlt import Position
 from hlt.positionals import Direction
 
-""" <<<Game Begin>>> """
-
 game = hlt.Game()
-# Ready, starts 2 second timer per turn.
-game.ready("Justin-AIS-Halite-V1")
+game.ready("Old-Bot")
 logging.info("Initialized! ID = {}.".format(game.my_id))
 
-""" <<<Game Loop>>> """
+ship_paths = {} #Dict with paths for each ship.
 
-longterm_targets = {} # dict with ship id as key, with target position and how many turns to get there.
-shortterm_targets = {} # dict with ship id as key, with next position stored.
+def find_best_position(ship):
+    optimal_position = ship.position
+    max_halite_value = game_map[ship.position].halite_amount
 
-def find_best_move(ship):
-    best_position = ship.position
-    best_value = 0
-    logging.info("finding best move...")
-    for y in range(game_map.height):
-        for x in range(game_map.width):
-            cell = game_map[hlt.Position(x, y)]
-            if not(cell.is_occupied) and (cell.position is not ship.position):
-                cell_value = cell.halite_amount / game_map.calculate_distance(ship.position, cell.position)
-                if cell_value > best_value:
-                    best_value = cell_value
-                    best_position = cell.position
+    current_targets = []
+    for path in ship_paths.values():
+        if len(path) > 0:
+            current_targets.append(path[len(path)-1])
+
+    for position in ship.position.get_surrounding_cardinals():
+        if position not in current_targets:
+            if game_map[position].halite_amount * 0.125 > max_halite_value * 0.25:
+                optimal_position = position
+                max_halite_value = game_map[position].halite_amount
+
+    if max_halite_value == 0:
+        max_halite_value = 0
+        for y in range(game_map.height):
+            for x in range(game_map.width):
+                cell = game_map[hlt.Position(x, y)]
+                if not cell.is_occupied and cell.position is not ship.position and cell.position not in current_targets:
+                    cell_value = cell.halite_amount / game_map.calculate_distance(ship.position, cell.position)
+                    if cell_value > max_halite_value:
+                        max_halite_value = cell_value
+                        optimal_position = cell.position
             
-    return best_position
+    return game_map.normalize(optimal_position)
+
+def a_star(start, target):
+    target_tuple = pos_to_tuple(target)
+    frontier = Queue()
+    frontier.put(pos_to_tuple(start))
+    came_from = {}
+    came_from[pos_to_tuple(start)] = None
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        if current == target_tuple:
+            break
+
+        for next in Position(current[0], current[1]).get_surrounding_cardinals():
+            if pos_to_tuple(next) not in came_from:
+                frontier.put(pos_to_tuple(next))
+                came_from[pos_to_tuple(next)] = current
+
+    current = pos_to_tuple(target)
+    path = []
+
+    while current != pos_to_tuple(start):
+        path.append(current)
+        current = came_from[current]
+    
+    path.append(pos_to_tuple(start))
+    path.reverse()
+
+    position_path = []
+
+    for position in path:
+        position_path.append(game_map.normalize(tuple_to_pos(position)))
+
+    if len(position_path) < 0:
+        position_path.append(game_map.normalize(target))
+
+    return position_path
+
+def pos_to_tuple(position):
+    return (position.x, position.y)
+
+def tuple_to_pos(tup):
+    return game_map.normalize(Position(tup[0], tup[1]))
+
+def can_move_off(ship):
+    if ship.halite_amount >= 0.10*game_map[ship.position].halite_amount:
+        return True
+    else:
+        return False
 
 while True:
     game.update_frame()
     me = game.me
     game_map = game.game_map
-    current_halite = me.halite_amount
 
-    # Holds commands, submitted at the end of turn. 
     command_queue = []
-    
+
+    next_positions = {}
+
+    players = game.players
+
+    opposing_locations = []
+    for player in players:
+        if player != game.my_id:
+            for ship in players[player].get_ships():
+                opposing_locations.append(ship.position)
+
+    logging.info("Current Paths (Turn Start): {}".format(ship_paths))
 
     for ship in me.get_ships():
+        has_current_path = False
+        for ship_id in ship_paths:
+            if ship_id == ship.id and len(ship_paths[ship.id]) > 0:
+                has_current_path = True
 
-
-        if (ship.halite_amount > 750) and not game_map[me.shipyard.position].is_occupied:
-            # if (me.halite_amount >= constants.DROPOFF_COST+5000) and (game_map.calculate_distance(ship.position, me.shipyard.position) > 30):
-            #     command_queue.append(ship.make_dropoff())
-            # else:
-                command_queue.append(ship.move(game_map.naive_navigate(ship, me.shipyard.position)))
-        else:
-            max_halite_position = ship.position
-            max_halite_value = game_map[ship.position].halite_amount
-
-            for position in ship.position.get_surrounding_cardinals():
-                if game_map[position].halite_amount * 0.125 > max_halite_value * 0.25:
-                    max_halite_position = position
-                    max_halite_value = game_map[position].halite_amount
-
-            if max_halite_value is 0:
-                max_halite_position = find_best_move(ship)
-
-            if (max_halite_position is not ship.position):
-                command_queue.append(ship.move(game_map.naive_navigate(ship, max_halite_position)))
+        if has_current_path == False:
+            if ship.halite_amount > 750 and not game_map[me.shipyard.position].is_occupied:
+                ship_paths[ship.id] = a_star(ship.position, me.shipyard.position)
             else:
-                command_queue.append(ship.stay_still())
+                ship_paths[ship.id] = a_star(ship.position, find_best_position(ship))
 
-    if game.turn_number <= 200 and me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied:
+    for ship_id in ship_paths:
+        if me.has_ship(ship_id):
+            if can_move_off(me.get_ship(ship_id)) and len(ship_paths[ship_id]) > 0:
+                next_positions[ship_id] = ship_paths[ship_id][0]
+            else:
+                next_positions[ship_id] = me.get_ship(ship_id).position
+    
+    logging.info("Next Positions: {}".format(next_positions))
+
+    next_adj_positions = {}
+
+    for ship_id in next_positions:
+        if next_positions[ship_id] == me.get_ship(ship_id).position:
+            next_adj_positions[ship_id] = me.get_ship(ship_id).position
+
+    for ship_id in next_positions:
+        if ship_id not in next_adj_positions:
+            if next_positions[ship_id] in next_adj_positions.values() or next_positions[ship_id] in opposing_locations:
+                if me.get_ship(ship_id).position in next_adj_positions.values():
+                    for position in me.get_ship(ship_id).position.get_surrounding_cardinals():
+                        if position not in next_adj_positions.values():
+                            ship_paths[ship_id].insert(0, me.get_ship(ship_id).position)
+                            next_adj_positions[ship_id] = game_map.normalize(position)
+                else:
+                    next_adj_positions[ship_id] = me.get_ship(ship_id).position
+            else:
+                next_adj_positions[ship_id] = next_positions[ship_id]
+
+    #     if next_positions[ship_id] == me.get_ship(ship_id).position:
+    #         next_adj_positions[ship_id] = me.get_ship(ship_id).position
+    #     elif next_positions[ship_id] not in next_adj_positions.values():
+    #         next_adj_positions[ship_id] = next_positions[ship_id]
+    #     else:
+    #         next_adj_positions[ship_id] = me.get_ship(ship_id).position
+            
+    # positions = []
+    # duplicate_positions = []
+    # for ship_id in next_adj_positions:
+    #     if pos_to_tuple(next_adj_positions[ship_id]) not in positions:
+    #         positions.append(pos_to_tuple(next_adj_positions[ship_id]))
+    #     else:
+    #         duplicate_positions.append(next_adj_positions[ship_id])
+    #         logging.info("Duplicate positions found: {}".format(duplicate_positions))
+
+    # duplicate_ships = []
+    # for duplicate in duplicate_positions:
+    #     for ship_id, position in next_adj_positions.items():    # for name, age in dictionary.iteritems():  (for Python 2.x)
+    #         if position == duplicate:
+    #             if next_adj_positions[ship_id] != me.get_ship(ship_id).position:
+    #                 next_adj_positions[ship_id] = me.get_ship(ship_id).position
+    #                 logging.info("Duplicate Avoided!")
+
+    # for ship in me.get_ships():
+    #     if ship.id not in ship_paths:
+    #         next_adj_positions[ship.id] = ship.position
+    #     elif ship.position == next_positions[ship.id]:
+    #         next_adj_positions[ship.id] = ship.position
+
+    # for ship_id in next_positions:
+    #     if next_positions[ship_id] not in next_adj_positions.values():
+    #         next_adj_positions[ship_id] = next_positions[ship_id]
+    #     else:
+    #         next_adj_positions[ship_id] = me.get_ship(ship_id).position
+
+    logging.info("Next Adjusted Positions: {}".format(next_adj_positions))
+
+    for ship_id in next_adj_positions:
+        choices = [Direction.North, Direction.South, Direction.West, Direction.East, Direction.Still]
+        for choice in choices:
+            if game_map.normalize(me.get_ship(ship_id).position.directional_offset(choice)) == game_map.normalize(next_adj_positions[ship_id]):
+                command_queue.append(me.get_ship(ship_id).move(choice))
+                if next_adj_positions[ship_id] == ship_paths[ship_id][0]:
+                    ship_paths[ship_id].pop(0)
+
+    if game.turn_number <= 200 and me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied and not me.shipyard.position in next_adj_positions.values():
         command_queue.append(me.shipyard.spawn())
-
-    # End turn.
+        
+    logging.info("Current Paths (Turn End): {}".format(ship_paths))
+    
     game.end_turn(command_queue)
